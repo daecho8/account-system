daf1bc4745ebfe5d2d36503107304b1900722cad
DaeyeonCho <daecho8@gmail.com>
2023-10-16 18:38:56 +0900

    global exception handler added

--- src/main/java/com/example/account/type/ErrorCode.java
+++ src/main/java/com/example/account/type/ErrorCode.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 
+    INTERNAL_SERVER_ERROR("내부 서버 오류가 발생했습니다."),
     USER_NOT_FOUND("사용자가 없습니다."),
     ACCOUNT_NOT_FOUND("계좌가 없습니다."),
     TRANSACTION_NOT_FOUND("해당 거래가 없습니다."),
